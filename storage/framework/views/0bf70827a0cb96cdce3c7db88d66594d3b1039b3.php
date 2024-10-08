000fd1bf0da664f2a789e8a77ec4960eab53c0d5
Omar Bouhdida <omar.bouhdida@sesame.com.tn>
2024-10-08 20:32:53 +0100

    Atelier I

---
+++ storage/framework/views/0bf70827a0cb96cdce3c7db88d66594d3b1039b3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Étudiants</title>
+</head>
+<body>
+    <h1>Liste des Étudiants</h1>
+    <?php echo e($nom); ?>
+
+    <?php echo e($prenom); ?>
+
+</body>
+</html><?php /**PATH C:\REP\TP\resources\views/etudiant.blade.php ENDPATH**/ ?>
